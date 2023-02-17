5720e7f329dc1ac6c9b74bc20624202cfbc9c344
elenahorton21 <elena.horton21@gmail.com>
2023-02-17 16:54:12 -0500

    grpc code basic functionality

--- WireProtocol/grpc/grpc_client.py
+++ WireProtocol/grpc/grpc_client.py
@@ -1,38 +1,109 @@
 """
 Python program to implement client side of chat application.
-
-TODO: How can the client know when the server has disconnected?
-TODO: Refactor to make cleaner.
 """
-import socket
-import select
 import sys
 import chat_pb2
 import chat_pb2_grpc
 import grpc
+from threading import Thread
+from time import sleep
 
 MAX_BUFFER_SIZE = 1024
 
+def handle_input(input, username, stub):
+    try: 
+        if input.startswith("/list"):
+            if len(input.split(" ")) == 1:
+                wildcard = None
+            else:
+                wildcard1 = input.split(" ")[1]
+                wildcard = wildcard1.replace('\n', "")
+            response = stub.list_users(chat_pb2.ListRequest(wildcard=wildcard))
+            print(response)
+            return
+        
+        # DELETE user case
+        elif input.startswith("/delete"):
+            input = input.strip()
+            user_to_delete1 = input.split(" ")[1]
+            user_to_delete = user_to_delete1.replace('\n',"")
+            response = stub.delete_user(chat_pb2.DeleteRequest(from_user = username, to_user = user_to_delete))
+            print(str(response))
+            return
+
+        # user requesting to send a direct message to a specified recipient
+        elif input.startswith(">>"):
+            msgList = input.split(":")
+            usernameToSend1 = msgList[0].replace(">", "")
+            usernameToSend = usernameToSend1.replace(" ", "")
+            msgText = msgList[1]
+            return stub.send_message(chat_pb2.MessageRequest(from_user=username, to_user=usernameToSend, message=msgText))
+
+        # user logging in as a different user
+        elif input.startswith("/login"):
+            input = input.strip()
+            username1 = input.split(" ")[1]
+            username = username1.replace('\n',"")
+            response = login(stub, username)
+            print(str(response))
+            return
+    
+        # broadcast message to all
+        else: 
+            return stub.send_message(chat_pb2.MessageRequest(from_user=username, to_user=None, message=input))
+        
+    except Exception:
+        print("Improper usage.")
+        print("(1) Send a message to all --> type message and press enter")
+        print("(2) Send a message to specified recipient --> '>> [recipient]: [message]'")
+        print("(3) List all recipients w/ optional wildcard --> '/list [wildcard]'")
+        print("(4) Delete a specified recipient account --> '/delete [recipient]'")
+        print("(5) Login as different user --> '/login [username]'")
+
+
+def login(stub, username):
+    response = stub.create_user(chat_pb2.UserRequest(username=username))
+    return response
+
+
+
+def getServerMessagesThread(stub, username):
+    while True:
+        # check constantly if we have inbound messages
+        response = stub.get_message(
+            chat_pb2.GetRequest(user = username))
+        if response.message == "LOGGED_OUT":
+            print("You have been deleted from the chat. Please login again.")
+        elif response.message != "NONE":
+            print(response.message)
+        sleep(.5)
+
+def startServerThread(chatStub, username):
+    t = Thread(target=getServerMessagesThread, args=(chatStub, username))
+    t.start()
+    return
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = chat_pb2_grpc.ChatStub(channel)
-        response = None
         username = input("Enter your username:")
-        response = stub.create_user(chat_pb2.UserRequest(username=username))
-        print(f"Welcome to the chatroom {username}!")
+        response = login(stub, username)
+        print(response)
+        startServerThread(stub, username)
+
+        # Display initial usage suggestions
+        print("")
+        print("USAGE:")
+        print("(1) Send a message to all --> type message and press enter")
+        print("(2) Send a message to specified recipient --> '>> [recipient]: [message]'")
+        print("(3) List all recipients w/ optional wildcard --> '/list [wildcard]'")
+        print("(4) Delete a specified recipient account --> '/delete [recipient]'")
+        print("(5) Login as different user --> '/login [username]'")
+
 
         while True:
-            sockets_list = [sys.stdin, stub]
-            read_sockets, write_socket, error_socket = select.select(sockets_list,[],[])
-            
-            for socks in read_sockets:
-                if socks == stub:
-                    print("INCOMING MESSAGE!")
-                else:
-                    newInput = sys.stdin.readline()
-                    print("READ! " + newInput)
-                
+            newInput = sys.stdin.readline()
+            handle_input(newInput, username, stub)
             
 
 if __name__ == "__main__":
@@ -45,5 +116,4 @@ if __name__ == "__main__":
     # port = int(sys.argv[2])
     # IP_address = "127.0.0.1"
     # port = 50015 # server's port
-
     run()
--- WireProtocol/grpc/grpc_server.py
+++ WireProtocol/grpc/grpc_server.py
@@ -4,46 +4,148 @@ import grpc
 import chat_pb2
 import chat_pb2_grpc
 from app import App
+import logging
+from config import config
 
 chatServer = App()
+
+# Logging config
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
+
+
+# Server config
+SERVER_HOST = config["SERVER_HOST"]
+SERVER_PORT = config["SERVER_PORT"]
+MAX_BUFFER_SIZE = config["MAX_BUFFER_SIZE"]
+MAX_NUM_CONNECTIONS = config["MAX_NUM_CONNECTIONS"]
 
 
 class Chat(chat_pb2_grpc.ChatServicer):
 
     def create_user(self, request, _context):
         username = request.username
-        print("New user: " + username)
+        print("Joining user: " + username)
         result = chatServer.create_user(username)
-        response = "success!" if result else "failed to create new user :("
+        response = str("Welcome to the chat " + username + "!")if result else str("Welcome back " + username + "!")
         return chat_pb2.ChatReply(message=response)
 
     # send message from one logged in user to another
     def send_message(self, request, _context):
-        return
+        from_user = request.from_user
+        to_user = request.to_user
+        msg = request.message
+        print("sending message from: " + from_user + " to: " + to_user)
+        result = chatServer.send_message(from_user, to_user, msg)
+        return chat_pb2.ChatReply(message = result)
 
-# list users matching with a wildcard
+
+    # list users matching with a wildcard
     def list_users(self, request, _context):
-        return
+        wildcard = request.wildcard
+        result = chatServer.list_users(wildcard)
 
-# read a message that has been sent to the logged in user
+        if result:
+            return chat_pb2.ChatReply(message = result)
+        else:
+            return chat_pb2.ChatReply(message= "Could not list users.")
+
+    # read a message that has been sent to the logged in user
     def get_message(self, request, _context):
-        return
+        username = request.user
+        msg = chatServer.get_messages(username)
+        if msg == 100:
+            return chat_pb2.ChatReply(message="LOGGED_OUT")
+        else:
+            return chat_pb2.ChatReply(message = msg)
 
-# delete user
+    # delete user
     def delete_user(self, request, _context):
-       return
+        user_to_delete = request.to_user
+        user_deleting = request.from_user
+        response = chatServer.delete_user(user_to_delete, user_deleting)
+        if response == True:
+            print("User " + user_to_delete + " deleted by " + user_deleting)
+            response = "Success."
+        return chat_pb2.ChatReply(message = response)
 
 
 
 def serve():
     port = '50051'
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
     chat_pb2_grpc.add_ChatServicer_to_server(Chat(), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
-    print("Server started, listening on " + port)
+    print("GRPC Server started, listening on " + port)
     server.wait_for_termination()
 
 
+
+####-------------------------------------------------------------------####
+
+
+# def _handle_chat_message(user, to_user, msg, app):
+#     """
+#     Handle a ChatMessage from client. Returns an error response if the recipient
+#     does not exist. Otherwise, broadcasts to active recipients, queues the 
+#     the message for inactive recipients, and returns a success response.
+
+#     Args:
+#         msg (ChatMessage): The message received from client.
+#         app (AppState): The current app state.
+    
+#     Returns:
+#         ChatResponse: The response to client.
+#     """
+#     # If the recipient does not exist, return an error response
+#     if to_user and not app.is_valid_user(to_user):
+#         res = str("User " + to_user + " does not exist.")
+#         return res
+
+#     # Convert ChatMessage to BroadcastMessage
+#     broadcast_msg = msg.to_broadcast() 
+#     # If recipient was not specified, all active users are recipients
+#     if not to_user:
+#         recv_conns = app.get_all_connections()
+#     # Otherwise, broadcast the message to sender, and recipient if active.
+#     else:
+#         # TODO: Roundabout way of getting sender socket    
+#         recv_conns = [app.get_user_connection(user)]
+#         # Get recipient socket
+#         recipient_conn = app.get_user_connection(to_user)
+#         # If recipient is active, add their socket to receiving connections
+#         if recipient_conn:
+#             recv_conns.append(recipient_conn)
+#         # If `recv_conn` is None, the user is inactive. Queue the message for later.
+#         else:
+#             app.queue_message(to_user, broadcast_msg)
+    
+#     # Broadcast the message to recipients
+#     # TODO: Are there cases where this fails and we should return error response?
+#     broadcast(broadcast_msg, recv_conns)
+
+#     # Return success response
+#     return True
+
+# def broadcast(msg, recvs):
+#     """
+#     Send a message to a list of clients.
+#     TODO: Handle exceptions with `conn.send`.
+
+#     Args:
+#         msg (BroadcastMessage): The message to send to clients.
+#         recvs (List[Socket]): The sockets to send the message to.
+
+#     Returns:
+#         None
+#     """
+#     # Check that the message is an instance of BroadcastMessage
+
+#     for conn in recvs:
+#         conn.send(msg.encode_())
+
 if __name__ == '__main__':
     serve()
+
+
