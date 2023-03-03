000cdb4d0ca9b7e4f2e2edd6e4d54f192b053881
elenahorton21 <elena.horton21@gmail.com>
2023-03-03 11:50:20 -0500

    baseline before adding logic

--- LogicalClock/logicalclock.py
+++ LogicalClock/logicalclock.py
@@ -2,23 +2,25 @@ import threading
 import queue
 import random
 
-# General idea, this program can create as many processes as we need 
-# through the threading library run threads that are acting as individual processes
+# General idea, threading library creates "processes" that are each running our logical clock module
 
 # the queue is shared by all the processes, includes messages for each thread
 
 class myThread (threading.Thread):
-    def __init__(self, id, queues, my_queue):
+    def __init__(self, id, queues, my_queue, clock):
       threading.Thread.__init__(self)
       self.id = id
       self.queues = queues
       self.my_queue = my_queue
+      self.clock = clock
 
     def add_to_queue(self, queueID, message):
         self.queues[queueID].put(message)
 
 
-    def run(self):              
+    def run(self):  
+
+    # add all the logic here for handling what to do on each clock tick             
       print("Starting " + str(self.id))
       print("Exiting " + str(self.id))
 
@@ -33,7 +35,7 @@ def run():
     clocktime = random.randint(1, 6)
     print(clocktime)
 
-    # Create three "model clock" processes
+    # Create three "model clock" processes, note their ID in the queues list
     p1 = myThread(1, queues, 0)
     p2 = myThread(2, queues, 1)
     p3 = myThread(3, queues, 2)
@@ -42,8 +44,10 @@ def run():
     p2.start()
     p3.start()
 
+
+    # testing functionality of the queues across processes --> working!
     p1.add_to_queue(2, "hi!")
-    p2. add_to_queue(1, "what's up? ")
+    p2.add_to_queue(1, "what's up? ")
 
     for i in range(0, len(queues)):
         q = queues[i]
